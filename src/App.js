8b3bbca818e0365dc1e0adad436dae27b9a8d676
Shamsullah Barak <87421301+shamsullah-shams@users.noreply.github.com>
2024-12-05 09:15:07 +0430

    new changes

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = () => {
   // return <RouterProvider router={router} />;
   return (
     <UserContext.Provider value={{ user, setUser }}>
-      <RouterProvider router={router} />;
+      <RouterProvider router={router} />
     </UserContext.Provider>
   );
 };
--- src/page/Quiz.jsx
+++ src/page/Quiz.jsx
@@ -1,11 +1,44 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { questions } from "../data/questions";
 
 const Quiz = () => {
   const [selected, setSelected] = useState(0);
+  const [current, setCurrent] = useState(0);
+  const [green, setGreen] = useState(false);
+  const [selectedQuestion, setSelectedQuestions] = useState([]);
+
+  useEffect(() => {
+    const getSelectedQuestions = () => {
+      const arr = [];
+
+      for (let i = 0; i < 10; i++) {
+        arr.push(Math.floor(Math.random() * 100) + 1);
+      }
+      return arr;
+    };
+
+    setSelectedQuestions(getSelectedQuestions());
+  }, []);
 
   // select handler
   const selectHandler = (number) => {
     setSelected(number);
+  };
+
+  const increase = () => {
+    setCurrent(current + 1);
+    setSelected(0);
+  };
+
+  const handleQuestion = (index) => {
+    const correctAnswer = questions[selectedQuestion[current]].answer;
+    const q = questions[selectedQuestion[current]].options[index];
+
+    if (q === correctAnswer) {
+      setSelected(index + 1);
+    } else {
+      setGreen(true);
+    }
   };
 
   return (
@@ -21,13 +54,49 @@ const Quiz = () => {
           borderRadius: "20px",
         }}
       >
-        <div style={{ marginLeft: "10%" }}>
-          <span style={{ fontWeight: "bold", color: "red" }}>4</span>/10
+        <div
+          style={{
+            marginLeft: "10%",
+            marginRight: "10%",
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <div>
+            <span style={{ fontWeight: "bold", color: "red" }}>
+              {current + 1}
+            </span>
+            /10
+          </div>
+          <div>
+            <span style={{ fontWeight: "bold", color: "red" }}>4</span>/10
+          </div>
         </div>
         <p style={{ marginLeft: "10%", fontSize: "24px", fontWeight: "bold" }}>
-          What is your university name?
+          {selectedQuestion.length > 0 && (
+            <>{questions[selectedQuestion[current]].question}</>
+          )}
         </p>
-        <div
+        {selectedQuestion.length > 0 &&
+          questions[selectedQuestion[current]].options.map((item, index) => {
+            return (
+              <div
+                key={index}
+                onClick={() => handleQuestion(index)}
+                style={{
+                  ...styles.choice,
+                  backgroundColor:
+                    green && selected === index + 1
+                      ? "red"
+                      : selected === index + 1 && "green",
+                  color: selected === index + 1 && "white",
+                }}
+              >
+                {item}
+              </div>
+            );
+          })}
+        {/* <div
           onClick={() => selectHandler(1)}
           style={{
             ...styles.choice,
@@ -66,7 +135,7 @@ const Quiz = () => {
           }}
         >
           Herat University
-        </div>
+        </div> */}
 
         <div
           style={{
@@ -84,6 +153,7 @@ const Quiz = () => {
               marginTop: "16px",
               margin: "3px",
             }}
+            onClick={increase}
           >
             Next
           </button>
